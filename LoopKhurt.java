2d9294b2f6dae43cd5c5f9d3f89b94920616abc2
KhurtQuibral <khurtquibral@gmail.com>
2024-10-04 14:26:14 +0800

    Add files via upload

---
+++ LoopKhurt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LoopKhurt
+{
+	public static void main (String[] args)
+	{
+		for (int i= 1; i <= 5000; ++i)
+		{
+			System.out.println("Quibral Khurt");
+		}
+	}
+}	
